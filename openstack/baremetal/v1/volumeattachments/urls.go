ba9fbec8aaedc97db7b87956ca73d21fc646cdee
yushangbin <shangbin.yu@easystack.cn>
2024-10-25 15:36:11 +0800

    [2]Modify urls for baremetal volumeattachments API
    Closes-Task: #EAS-129131

--- openstack/baremetal/v1/volumeattachments/urls.go
+++ openstack/baremetal/v1/volumeattachments/urls.go
@@ -3,9 +3,9 @@ package volumeattachments
 import "github.com/gophercloud/gophercloud"
 
 func createURL(client *gophercloud.ServiceClient, nodeID string) string {
-	return client.ServiceURL("nodes" + nodeID + "/volume_attachments")
+	return client.ServiceURL("nodes/" + nodeID + "/volume_attachments")
 }
 
 func deleteURL(client *gophercloud.ServiceClient, nodeID string, volumeID string) string {
-	return client.ServiceURL("nodes" + nodeID + "/volume_attachments/" + volumeID)
+	return client.ServiceURL("nodes/" + nodeID + "/volume_attachments/" + volumeID)
 }
